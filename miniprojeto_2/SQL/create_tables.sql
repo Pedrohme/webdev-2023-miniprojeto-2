c5f2a3aa3d64ccf38026230566f4991b4aba3e14
Pedrohme <pedrohmen45@gmail.com>
2023-03-04 15:45:46 -0300

    ajustes do script

--- miniprojeto_2/SQL/create_tables.sql
+++ miniprojeto_2/SQL/create_tables.sql
@@ -2,10 +2,12 @@ CREATE TABLE IF NOT EXISTS ingredientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL
 );
+
 CREATE TABLE IF NOT EXISTS lanches (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL
 );
+
 CREATE TABLE IF NOT EXISTS lanche_ingredientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     id_ingrediente INT,
@@ -13,10 +15,15 @@ CREATE TABLE IF NOT EXISTS lanche_ingredientes (
     FOREIGN KEY (id_lanche) REFERENCES lanches (id) ON UPDATE RESTRICT ON DELETE CASCADE,
     FOREIGN KEY (id_ingrediente) REFERENCES ingredientes (id) ON UPDATE RESTRICT ON DELETE CASCADE
 );
+
 CREATE TABLE IF NOT EXISTS pedidos (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome_cliente VARCHAR(255) NOT NULL
+    nome_cliente VARCHAR(255) NOT NULL,
+    rua VARCHAR(255) NOT NULL,
+    bairro VARCHAR(255) NOT NULL,
+    numero VARCHAR(255) NOT NULL
 );
+
 CREATE TABLE IF NOT EXISTS lanche_pedido (
     id INT AUTO_INCREMENT PRIMARY KEY,
     id_lanche INT,
@@ -24,6 +31,7 @@ CREATE TABLE IF NOT EXISTS lanche_pedido (
     FOREIGN KEY (id_lanche) REFERENCES lanches (id) ON UPDATE RESTRICT ON DELETE CASCADE,
     FOREIGN KEY (id_pedido) REFERENCES pedidos (id) ON UPDATE RESTRICT ON DELETE CASCADE
 );
+
 CREATE TABLE IF NOT EXISTS lanche_pedido_ingredientes_adicionados (
     id INT AUTO_INCREMENT PRIMARY KEY,
     id_lanche_pedido INT,
@@ -31,6 +39,7 @@ CREATE TABLE IF NOT EXISTS lanche_pedido_ingredientes_adicionados (
     FOREIGN KEY (id_lanche_pedido) REFERENCES lanche_pedido(id) ON UPDATE RESTRICT ON DELETE CASCADE,
     FOREIGN KEY (id_ingrediente) REFERENCES ingredientes(id) ON UPDATE RESTRICT ON DELETE CASCADE
 );
+
 CREATE TABLE IF NOT EXISTS lanche_pedido_ingredientes_removidos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     id_lanche_pedido INT,
