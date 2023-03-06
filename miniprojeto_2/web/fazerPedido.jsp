05f554ec3bd8e2be405f42ae8fb3e29b13862b0e
GuiMilani <gui.milani@hotmail.com>
2023-03-06 01:00:52 -0300

    manipulacao de pedidos melhorada

--- miniprojeto_2/web/fazerPedido.jsp
+++ miniprojeto_2/web/fazerPedido.jsp
@@ -27,7 +27,18 @@
 <main>
     <h1 class="title">Chess Burger</h1>
     <h2 class="subtitle">Escolha seus lanches</h2>
-    <ul style="display: none;" id="adicionais"></ul>
+    <ul style="display: none;" id="adicionais">
+        <% for (Ingrediente ingrediente : ingredientes) { %>
+            <jsp:element name="li">
+                <jsp:attribute name="data-id">
+                    <%=ingrediente.getId()%>
+                </jsp:attribute>
+                <jsp:attribute name="data-nome">
+                    <%=ingrediente.getNome()%>
+                </jsp:attribute>
+            </jsp:element>
+        <%}%>
+    </ul>
     <ul class="cards">
         <% for (Lanche lanche : lanches) { %>
         <li>
@@ -78,44 +89,6 @@
     </ul>
     <h2 class="subtitle">Seu pedido</h2>
     <ul class="cards" id="pedidos">
-        <li>
-            <section class="card">
-                <h3 class="card-title">{Nome do lanche}</h3>
-                <h4 class="card-subtitle">Adicionais:</h4>
-                <ul>
-                    <li class="list-item">{Adicional 1}</li>
-                </ul>
-                <h4 class="card-subtitle">Removidos:</h4>
-                <ul>
-                    <li class="list-item">{Ingrediente 1}</li>
-                </ul>
-                <article>
-                    <h4 class="card-subtitle">
-                        Que tal subir o rating do seu lanche?
-                    </h4>
-                    <ul>
-                        <li>
-                            <p class="list-item">{Adicional 1}</p>
-                            <button class="btn">Adicionar</button>
-                        </li>
-                    </ul>
-                </article>
-                <article>
-                    <h4 class="card-subtitle">
-                        Deseja remover alguma peça dessa posição?
-                    </h4>
-                    <ul>
-                        <li>
-                            <p class="list-item">{Ingrediente 1}</p>
-                            <button class="btn">Remover</button>
-                        </li>
-                    </ul>
-                </article>
-                <div class="center">
-                    <button class="btn">Excluir do pedido</button>
-                </div>
-            </section>
-        </li>
     </ul>
     <div class="btn-div">
         <button class="btn">Cancelar pedido</button>
--- miniprojeto_2/web/scripts/adicionar.js
+++ miniprojeto_2/web/scripts/adicionar.js
@@ -1,66 +1,80 @@
 $(() => {
-  const addLancheButton = $('.add-lanche');
-
-  addLancheButton.on('click', addLanche);
-});
-
-const pedidos = $('#pedidos')
-const itensPedido = [];
-
-function addLanche(event) {
-  event.preventDefault();
-
-  const lanche = {
-    nome: $(this).data('nome'),
-    ingredientes: $.map($(`[data-lanche="${$(this).data('id')}"]`), function(n) {
+    const addLancheButton = $('.add-lanche');
+  
+    addLancheButton.on('click', addLanche);
+  });
+  
+  const itensPedido = [];
+  
+  function getAdicionais() {
+    return $.map($('#adicionais li'), function(n) {
       const ingrediente = {
         id: n.attributes[1].value,
-        nome: n.innerText.trim()
+        nome: n.attributes[0].value,
       };
       return ingrediente;
-    })
+    });
   };
   
-  itensPedido.push(lanche);
-
-  $('#pedidos').append(`
-  <li>
-      <section class="card">
-          <h3 class="card-title">{Nome do lanche}</h3>
-          <h4 class="card-subtitle">Adicionais:</h4>
-          <ul>
-              <li class="list-item">{Adicional 1}</li>
-          </ul>
-          <h4 class="card-subtitle">Removidos:</h4>
-          <ul>
-              <li class="list-item">{Ingrediente 1}</li>
-          </ul>
-          <article>
-              <h4 class="card-subtitle">
-                  Que tal subir o rating do seu lanche?
-              </h4>
-              <ul>
-                  <li>
-                      <p class="list-item">{Adicional 1}</p>
-                      <button class="btn">Adicionar</button>
-                  </li>
-              </ul>
-          </article>
-          <article>
-              <h4 class="card-subtitle">
-                  Deseja remover alguma peça dessa posição?
-              </h4>
-              <ul>
-                  <li>
-                      <p class="list-item">{Ingrediente 1}</p>
-                      <button class="btn">Remover</button>
-                  </li>
-              </ul>
-          </article>
-          <div class="center">
-              <button class="btn">Excluir do pedido</button>
-          </div>
-      </section>
-  </li>
-  `)
-}
+  function addLanche(event) {
+    event.preventDefault();
+  
+    const lanche = {
+      nome: $(this).data('nome'),
+      ingredientes: $.map($(`[data-lanche="${$(this).data('id')}"]`), function(n) {
+        const ingrediente = {
+          id: n.attributes[1].value,
+          nome: n.innerText.trim()
+        };
+        return ingrediente;
+      })
+    };
+    
+    itensPedido.push(lanche);
+  
+    getAdicionais()
+  
+    // TODO: transformar adicionais em li's e colocar na linha 56. não esquecer de adicionar classe específica no button. Depois fazer o mesmo para os ingredientes do lanche.
+  
+    $('#pedidos').append(`
+    <li>
+        <section class="card">
+            <h3 class="card-title">${lanche.nome}</h3>
+            <h4 class="card-subtitle">Adicionais:</h4>
+            <ul>
+                <li class="list-item">{Adicional 1}</li>
+            </ul>
+            <h4 class="card-subtitle">Removidos:</h4>
+            <ul>
+                <li class="list-item">{Ingrediente 1}</li>
+            </ul>
+            <article>
+                <h4 class="card-subtitle">
+                    Que tal subir o rating do seu lanche?
+                </h4>
+                <ul>
+                    <li>
+                        <p class="list-item">{Adicional 1}</p>
+                        <button class="btn">Adicionar</button>
+                    </li>
+                </ul>
+            </article>
+            <article>
+                <h4 class="card-subtitle">
+                    Deseja remover alguma peça dessa posição?
+                </h4>
+                <ul>
+                    <li>
+                        <p class="list-item">{Ingrediente 1}</p>
+                        <button class="btn">Remover</button>
+                    </li>
+                </ul>
+            </article>
+            <div class="center">
+                <button class="btn">Excluir do pedido</button>
+            </div>
+        </section>
+    </li>
+    `)
+  }
+  
